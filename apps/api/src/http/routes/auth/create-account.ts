01bb9fab20a30206c8a928463f009fd3a1312ed1
Arthur Willame <163607700+ArthurWillameBr@users.noreply.github.com>
2025-06-23 21:21:12 -0300

    feat(auth): implementar rota de criação de conta com validação de dados e verificação de e-mail existente

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,17 +1,50 @@
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod";
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { hash } from "bcryptjs"
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().post("users", {
-        schema: {
-            body: z.object({
-                message: z.string(),
-            })
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Create a new account',
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body as { name: string, email: string, password: string }
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          message: 'User with same e-mail already exists.',
+        })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: { 
+          name,
+          email,
+          passwordHash,
         }
-    },() => {
-        return {
-            message: "Hello World"
-        }
-    } )
+      })
+
+      return reply.status(201).send()
+    },
+  )
 }
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,17 +1,20 @@
-import { fastify } from "fastify"
 import fastifyCors from "@fastify/cors"
-import { 
-    jsonSchemaTransform, 
-    serializerCompiler, 
-    validatorCompiler, 
-    ZodTypeProvider  
-} from "fastify-type-provider-zod"
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createAccount } from "./routes/auth/create-account"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
 app.register(fastifyCors)
+
+app.register(createAccount)
 
 app.listen({
     port: 3333,
