5adab8e3651b3fed0a775f8f2bce8ba5e375918c
Arthur Willame <163607700+ArthurWillameBr@users.noreply.github.com>
2025-06-28 19:11:43 -0300

    fix zod type provider

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,9 +1,15 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
-import { hash } from "bcryptjs"
 import { z } from 'zod'
 
 import { prisma } from '@/lib/prisma'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -12,15 +18,19 @@ export async function createAccount(app: FastifyInstance) {
       schema: {
         tags: ['Auth'],
         summary: 'Create a new account',
-        body: z.object({
-          name: z.string(),
-          email: z.string().email(),
-          password: z.string().min(6),
-        }),
+        body: createAccountBodySchema,
+        response: {
+          201: z.object({
+            message: z.string(),
+          }),
+          400: z.object({
+            message: z.string(),
+          }),
+        },
       },
     },
     async (request, reply) => {
-      const { name, email, password } = request.body as { name: string, email: string, password: string }
+      const { name, email, password } = request.body 
 
       const userWithSameEmail = await prisma.user.findUnique({
         where: {
@@ -30,21 +40,23 @@ export async function createAccount(app: FastifyInstance) {
 
       if (userWithSameEmail) {
         return reply.status(400).send({
-          message: 'User with same e-mail already exists.',
+          message: 'User with same e-mail already exists.'
         })
       }
 
       const passwordHash = await hash(password, 6)
 
       await prisma.user.create({
-        data: { 
+        data: {
           name,
           email,
           passwordHash,
-        }
+        },
       })
 
-      return reply.status(201).send()
+      return reply.status(201).send({
+        message: 'User created successfully.'
+      })
     },
   )
 }
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,23 +1,43 @@
-import fastifyCors from "@fastify/cors"
-import fastify from 'fastify'
+import fastify from 'fastify';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
-  ZodTypeProvider,
-} from 'fastify-type-provider-zod'
-import { createAccount } from "./routes/auth/create-account"
+} from 'fastify-type-provider-zod';
+import { createAccount } from './routes/auth/create-account';
+import fastifyCors from '@fastify/cors';
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'SampleApi',
+      description: 'Sample backend service',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+
+  // You can also create transform with custom skiplist of endpoints that should not be included in the specification:
+  //
+  // transform: createJsonSchemaTransform({
+  //   skipList: [ '/documentation/static/*' ]
+  // })
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/documentation',
+});
 
 app.register(fastifyCors)
-
 app.register(createAccount)
 
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log("HTTP server running!")
+app.listen({ port: 4949, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
 })
