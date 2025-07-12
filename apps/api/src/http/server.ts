06c7c1e69b34e4d4e20dde4d8455a4186efcac6c
ArthurWillameBr <arthurwillame2005@gmail.com>
2025-07-11 21:50:26 -0300

    feat(auth): add password recovery and reset routes with validation and token handling

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,24 +1,26 @@
 import fastifyCors from "@fastify/cors";
+import fastifyJwt from "@fastify/jwt";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
-import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import {
 	jsonSchemaTransform,
 	serializerCompiler,
 	validatorCompiler,
 } from "fastify-type-provider-zod";
-import { createAccount } from "./routes/auth/create-account";
-import { authenticateWithPassword } from "./routes/auth/authenticate-with-password";
-import { getProfile } from "./routes/auth/get-profile";
 import { errorHandler } from "./error-handler";
+import { authenticateWithPassword } from "./routes/auth/authenticate-with-password";
+import { createAccount } from "./routes/auth/create-account";
+import { getProfile } from "./routes/auth/get-profile";
+import { requestPasswordRecover } from "./routes/auth/request-password-recover";
+import { resetPassword } from "./routes/auth/reset-password";
 
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.setErrorHandler(errorHandler)
+app.setErrorHandler(errorHandler);
 
 app.register(fastifySwagger, {
 	openapi: {
@@ -50,7 +52,8 @@ app.register(fastifyCors);
 app.register(createAccount);
 app.register(authenticateWithPassword);
 app.register(getProfile);
-
+app.register(requestPasswordRecover);
+app.register(resetPassword);
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
 	console.log("HTTP server running!");
 });
