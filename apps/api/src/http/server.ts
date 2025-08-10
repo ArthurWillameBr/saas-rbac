1aa767bb7fadbf53905b8b8a759fa6e0540b122f
ArthurWillameBr <arthurwillame2005@gmail.com>
2025-08-10 10:35:34 -0300

    feat(orgs): add organization routes for creation and retrieval
    Add new routes for creating organizations and retrieving organization data including:
    - createOrganization
    - getMembership
    - getOrganization
    - getOrganizations
    
    These changes enable users to manage organizations through the API.

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -15,6 +15,10 @@ import { createAccount } from "./routes/auth/create-account";
 import { getProfile } from "./routes/auth/get-profile";
 import { requestPasswordRecover } from "./routes/auth/request-password-recover";
 import { resetPassword } from "./routes/auth/reset-password";
+import { createOrganization } from "./routes/orgs/create-org";
+import { getMembership } from "./routes/orgs/get-membership";
+import { getOrganization } from "./routes/orgs/get-organization";
+import { getOrganizations } from "./routes/orgs/get-organizations";
 
 const app = fastify();
 
@@ -61,6 +65,11 @@ app.register(getProfile);
 app.register(requestPasswordRecover);
 app.register(resetPassword);
 app.register(authenticateWithGithub);
+
+app.register(createOrganization);
+app.register(getMembership);
+app.register(getOrganization);
+app.register(getOrganizations);
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
 	console.log("HTTP server running!");
